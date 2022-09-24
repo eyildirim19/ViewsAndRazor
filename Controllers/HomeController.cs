cb660897f82523967ad60224e529138e689234ec
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2022-09-24 13:34:20 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewsAndRazor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+           return View();
+        }
+
+        public IActionResult CarpimTablosu()
+        {
+            // Not : eğer view metoduna parametre ile viewname belirtmezsek action name isimli view dosyasını arar...
+            return View("Sonuc");
+        }
+    }
+}
